abd17646b17fc98bce3e897c7b8b92d7746c8b10
mjdiazdev <mjdiaz.dev@gmail.com>
2026-01-27 08:22:38 -0400

    Resolviendo redireccion del qr

--- backend/app/Handlers/CategoryHandler.php
+++ backend/app/Handlers/CategoryHandler.php
@@ -37,8 +37,9 @@ class CategoryHandler
         );
         $writer = new Writer($renderer);
 
-        $frontendUrl = env('FRONTEND_URL');
-        $qrContent = $frontendUrl . '/store?qr=' . $category->code;
+        //$frontendUrl = env('FRONTEND_URL', 'http://72.61.111.16');
+        //$qrContent = $frontendUrl . '/store?qr=' . $category->code;
+        $qrContent = 'http://72.61.111.16/store?qr=' . $category->code;
 
         // Generar SVG del QR
         $qrImage = $writer->writeString($qrContent);
